80a886b55dc952f841a2c2086a91f3a510e76638
TheMerjin <sreekarc11@gmail.com>
2025-08-14 18:53:46 -0700

    prod fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'hybrid',  // Choose the output mode for your project
+  integrations: [
+    react()  // Integrating React into Astro
+  ],
+  adapter: vercel({
+    edgeMiddleware: true,  // Enabling edge middleware for Vercel
+  }),
 });
